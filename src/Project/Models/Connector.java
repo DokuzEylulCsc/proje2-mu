2aa9d47bbf98f2d222fdf615b2801a955c262c21
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-05-25 21:06:04 +0300

    Add Project.Models.Connector

---
+++ src/Project/Models/Connector.java
@@ -0,0 +1,44 @@
+package Project.Models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+/**
+ * Singleton class.
+ */
+public class Connector {
+    private static Connector    ourInstance = new Connector();
+    private Connection          connection = null;
+
+    private Connector() {
+        connect();
+    }
+
+    /**
+     * Connect SQLite database at ./Project.db.
+     * If not exists, create default schema.
+     *
+     * TODO: Create default schema from scripts/schema.sql
+     */
+    public void connect() {
+        try {
+            setConnection(DriverManager.getConnection("jdbc:sqlite:Project.db"));
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static Connector getInstance() {
+        return ourInstance;
+    }
+
+    private void setConnection(Connection c) {
+        this.connection = c;
+    }
+
+    private Connection getConnection() {
+        return this.connection;
+    }
+}
