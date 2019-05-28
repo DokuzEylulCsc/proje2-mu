406a72a26e099e2d48bb3713b0f97fda69e4d67b
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-05-29 02:10:30 +0300

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
+
+# Create executable jar including libraries:
+#   Compiles all packages in src
+#   Uses manifest file at bin/Manifest.txt
+#   Uses libraries at bin/libraries
+#
+# Note: This script meant to be in $project_dir/bin, invoke from anywhere.
+
 jarname="project.jar"
 dir=$(dirname $0)
 
 echo "*** Compiling source code..."
-javac -d $dir -verbose $dir/../src/Project/*.java $dir/../src/Project/*/*.java
+javac -d $dir -verbose $dir/../src/*/*.java $dir/../src/*/*/*.java
 
 echo "*** Creating jarfile..."
-jar cfmv $jarname $dir/Manifest.txt $dir/Project $dir/libraries
+jar cfmv $jarname $dir/Manifest.txt $dir/*/
 
 rm -rf $dir/Project 
 echo "*** Success, written jarfile at $dir/$jarname "
