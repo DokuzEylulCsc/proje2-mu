e1950fe3b26952ab39b471186895ae292bc63e74
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-05-29 04:17:13 +0300

    Fix build.sh

--- res/build.sh
+++ res/build.sh
@@ -7,15 +7,18 @@
 #   Writes jar file to bin
 #
 # Note: This script meant to be in $project_dir/res, invoke from anywhere.
-
 jarname="Project.jar"
+
 dir=$(dirname $0)
+cd $dir # $project_dir/res
 
 echo "*** Compiling source code..."
-javac -d $dir/../bin -verbose $dir/../src/*/*.java $dir/../src/*/*/*.java
+javac -d ../bin -verbose  ../src/*/*.java ../src/*/*/*.java
 
 echo "*** Creating jarfile..."
-jar cfmv $dir/../bin/$jarname $dir/../Manifest.txt $dir/../bin/*
+cd ../bin
+jar cfmv $jarname ../Manifest.txt ./*
+cd ../lib
+jar ufv ../bin/$jarname *
 
 echo "*** Success, written jarfile at bin/$jarname "
-
