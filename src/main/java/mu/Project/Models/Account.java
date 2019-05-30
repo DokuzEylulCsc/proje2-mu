44cc0b044e7e1fa221a7c049494d52f985a8ff29
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-05-31 02:04:23 +0300

    Update Models.Account.save, insert to database

--- src/main/java/mu/Project/Models/Account.java
+++ src/main/java/mu/Project/Models/Account.java
@@ -31,12 +31,32 @@ public class Account implements Model {
      * Create Account object, do not save to database till save method is called.
      *
      * @param email String
-     * @param password_hash Integer
+     * @param password String
      * @param name String
      * @param admin Integer, 0 or 1
      * @throws if email already exists in database
      */
-    public Account(String email, Integer password_hash, String name, Integer admin) {
+    public Account(String email, String password, String name, Integer admin) {
+
+        if (admin != 0 && admin != 1) {
+            throw new UnsupportedOperationException("Admin can be only 0 or 1!");
+        }
+
+        this.email = email;
+        this.password_hash = hashPassword(password);
+        this.name = name;
+        this.admin = admin;
+    }
+
+    /**
+     * Initialize with password_hash from database.
+     *
+     * @param email String
+     * @param password_hash Integer
+     * @param name String
+     * @param admin Integer, 0 or 1
+     */
+    private Account(String email, Integer password_hash, String name, Integer admin) {
 
         if (admin != 0 && admin != 1) {
             throw new UnsupportedOperationException("Admin can be only 0 or 1!");
@@ -81,14 +101,15 @@ public class Account implements Model {
             ResultSet rs = stmt.executeQuery(query);
 
             rs.next();
-            return (
-                    new Account(
-                            rs.getString("email"),
-                            rs.getInt("password_hash"),
-                            rs.getString("name"),
-                            rs.getInt("admin")
-                    )
+            Account result = new Account(
+                    rs.getString("email"),
+                    rs.getInt("password_hash"),
+                    rs.getString("name"),
+                    rs.getInt("admin")
             );
+            rs.close();
+
+            return result;
 
         } catch (SQLException e) {
             Logger.getInstance().addLog(e);
@@ -99,7 +120,6 @@ public class Account implements Model {
 
     /**
      * Find account in table, if not exists create new.
-     * Do not leave id attribute empty.
      *
      * @param email String
      * @param password_hash Integer
@@ -114,7 +134,7 @@ public class Account implements Model {
             try (PreparedStatement pstmt = Connector.getInstance().getConnection().prepareStatement(insertQuery)) {
                 pstmt.setString(1, email);
                 pstmt.setInt(2, password_hash);
-                pstmt.setString(3, name);
+                pstmt.setString(3, (name != null) ? name : "None");
                 pstmt.setInt(4, admin);
                 pstmt.execute();
 
@@ -157,19 +177,22 @@ public class Account implements Model {
 
     /**
      * Update object in database.
-     * TODO: Insert if not exists.
      */
     public void save() {
 
-        try (PreparedStatement pstmt = Connector.getInstance().getConnection().prepareStatement(updateQuery)) {
-            pstmt.setString(1, getEmail());
-            pstmt.setInt(2, getPassword_hash());
-            pstmt.setString(3, getName());
-            pstmt.setString(4, getEmail());
-            pstmt.executeUpdate();
+        if (getAccount(getEmail()) == null) {
+            createAccount(getEmail(), getPassword_hash(), getName(), (isAdmin() ? 1 : 0));
+        } else {
+            try (PreparedStatement pstmt = Connector.getInstance().getConnection().prepareStatement(updateQuery)) {
+                pstmt.setString(1, getEmail());
+                pstmt.setInt(2, getPassword_hash());
+                pstmt.setString(3, getName());
+                pstmt.setString(4, getEmail());
+                pstmt.executeUpdate();
 
-        } catch (SQLException e) {
-            Logger.getInstance().addLog(e);
+            } catch (SQLException e) {
+                Logger.getInstance().addLog(e);
+            }
         }
     }
 
@@ -198,12 +221,16 @@ public class Account implements Model {
         return name;
     }
 
+    private static Integer hashPassword(String password) {
+        return password.hashCode();
+    }
+
     private Integer getPassword_hash() {
         return password_hash;
     }
 
-    public Boolean comparePasswordHash(Integer password_hash) {
-        return getPassword_hash().equals(password_hash);
+    public Boolean comparePassword(String password) {
+        return getPassword_hash().equals(hashPassword(password));
     }
 
     @Override
--- src/main/java/mu/Project/Models/Model.java
+++ src/main/java/mu/Project/Models/Model.java
@@ -1,7 +1,15 @@
 package mu.Project.Models;
 
 public interface Model {
+
+    /**
+     * Insert or update to database.
+     */
     void save();
+
+    /**
+     * Delete from database if exists.
+     */
     void delete();
 
 }
