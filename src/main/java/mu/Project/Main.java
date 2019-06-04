2e10603c86c2f0a9637eb01fda7e73874fcb78d7
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-06-04 03:15:12 +0300

    Update Main, log in to CustomerView instantly

--- src/main/java/mu/Project/Main.java
+++ src/main/java/mu/Project/Main.java
@@ -1,12 +1,22 @@
 package mu.Project;
 
+import mu.Project.Controllers.CustomerController;
 import mu.Project.Controllers.MainController;
+import mu.Project.Models.Account;
+import mu.Project.Views.CustomerView;
 
 public class Main {
     public static void main(String[] args) {
-        Connector.getInstance().connect();  // initialize jdbc
 
+
+        Connector.getInstance().connect();  // initialize jdbc
         MainController mainController = new MainController();
-        mainController.runApp();
+        try {
+            new CustomerView(new CustomerController(new Account("kestos@catmail.com", "password"), mainController));
+        } catch (Exception e) {
+            Logger.getInstance().addLog("CustomerView in Main not initialized.");
+            e.printStackTrace();
+        }
+        // mainController.runApp();
     }
 }
