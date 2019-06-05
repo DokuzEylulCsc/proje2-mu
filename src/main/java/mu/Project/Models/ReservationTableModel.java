a5a00016d518af411d323706cd80329cb58dfdf1
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-06-05 03:14:48 +0300

    Update ReservationTableModel, make uneditable and standard

--- src/main/java/mu/Project/Models/ReservationTableModel.java
+++ src/main/java/mu/Project/Models/ReservationTableModel.java
@@ -6,13 +6,35 @@ import javax.swing.table.DefaultTableModel;
 import java.util.Vector;
 
 public class ReservationTableModel extends DefaultTableModel {
+    private static Vector<String> columnNames = new Vector<>() {{
+        add("Hotel Name");
+        add("Stars");
+        add("Room Type");
+        add("Room Number");
+        add("Price");
+        add("Bed Space");
+        add("Sea View");
+        add("Safe");
+        add("Air Conditioners");
+        add("Televisions");
+        add("Minibars");
+        add("Extra Services");
+    }};
 
-    public ReservationTableModel(Vector<Vector<Object>> data, Vector<String> columnNames) {
+    public ReservationTableModel(Vector<Vector<Object>> data) {
         super(data, columnNames);
     }
 
     public Integer getSelectedRoomsId() {
         throw new NotImplementedException();
+    }
+
+    /**
+     * Make table read-only.
+     */
+    @Override
+    public boolean isCellEditable(int x, int y) {
+        return false;
     }
 
 }
